f1581e49e0687fd0d37dce5109779379ec95b7a4
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-23 11:43:02 -0300

    Js-course

--- exercicio01/script.js
+++ exercicio01/script.js
@@ -1,1 +1,10 @@
-window.alert('Seja bem vindo, pequeno gafanhoto');
+let botao = document.getElementById('trocaCor');
+let tagh2 = document.querySelector('h2')
+
+
+
+let funcao = function clicar(){
+    tagh2.style.color = 'red'
+}
+
+botao.addEventListener('click', funcao)
