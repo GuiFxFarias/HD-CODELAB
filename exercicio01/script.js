5c79ed8e245182661ee9540fdb4f7773bbf558ee
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-27 08:29:38 -0300

    formatation with prettier

--- exercicio01/script.js
+++ exercicio01/script.js
@@ -1,10 +1,8 @@
-let botao = document.getElementById('trocaCor');
-let tagh2 = document.querySelector('h2')
+let botao = document.getElementById("trocaCor");
+let tagh2 = document.querySelector("h2");
 
+let funcao = function clicar() {
+  tagh2.style.color = "red";
+};
 
-
-let funcao = function clicar(){
-    tagh2.style.color = 'red'
-}
-
-botao.addEventListener('click', funcao)
+botao.addEventListener("click", funcao);
