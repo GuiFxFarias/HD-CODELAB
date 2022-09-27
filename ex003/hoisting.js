a11c13cfcc23288c6306e9209d411b098eb0ad67
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-27 09:44:10 -0300

    studying

--- ex003/hoisting.js
+++ ex003/hoisting.js
@@ -1,9 +1,7 @@
 ano = 2000;
 
-console.log(ano)
+var ano;
 
-var ano = 2022;
-
-console.log(ano)
+console.log(ano);
 
 // Hoisting iça a variável em relação a declaração, não em relação à inicialização
---
+++ ex003/var-let-const.js
@@ -0,0 +1,19 @@
+let produto = "Playstation 5";
+var outroProdruto = "Xbox Series X";
+
+//console.log(produto)
+
+//console.log(outroProdruto)
+
+const n1 = 25;
+let n2 = 20;
+
+const carros = ["Ferrari", "Porsche", "Audi"];
+
+carros.push("Fusca");
+
+if (n1 > n2) {
+  var mensagem = "Hello World";
+}
+
+console.log(carros);
