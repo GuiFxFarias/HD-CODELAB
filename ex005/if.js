241286cc150113a39b4202c81c15606b9a7eef79
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-28 10:51:10 -0300

    if-ternario

---
+++ ex005/if.js
@@ -0,0 +1,11 @@
+const valor = 100;
+
+if (valor === "100") {
+  console.log("é vdd");
+} else if (valor < 200) {
+  console.log("isso tb é vdd");
+} else if (valor == 100) {
+  console.log("vdd na segunda");
+} else {
+  console.log("nao é vdd");
+}
---
+++ ex005/operador-ternario.js
@@ -0,0 +1,14 @@
+const hora = 16;
+
+if (hora < 18) {
+  console.log("Dia");
+} else {
+  console.log("Noite");
+}
+
+const hour = 18;
+
+// Short if on the line below
+const periodo = hour < 18 ? "Dia" : "Noite";
+
+console.log(periodo);
