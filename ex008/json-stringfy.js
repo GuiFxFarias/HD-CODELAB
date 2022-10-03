15fb66b59dadd698e29d6912a9f9cf10346efcfa
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-03 10:58:47 -0300

    stringfy

---
+++ ex008/json-stringfy.js
@@ -0,0 +1,13 @@
+const pessoa = {
+  primeiroNome: "Manuela",
+  ultimoNome: "San",
+  coresFav: ["vermelho", "preto", "roxo"],
+  idade: 15,
+  mostrarNome: function(){
+    return `${this.primeiroNome} ${this.ultimoNome}`
+  }
+};
+
+console.log(pessoa);
+console.log(pessoa.mostrarNome())
+console.log(JSON.stringify(pessoa, null, 4)); // Transforma em uma string
