233b292b45cf0b7aa92c5597b228c572bb3f68ce
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-03 11:23:03 -0300

    JSON

--- ex008/object.js
+++ ex008/object.js
@@ -9,10 +9,23 @@ const person = {
   idade: 19,
 };
 
-const union = Object.assign(user, person, { // Obejto do lado direito tem preferencia, se houver conflitos
-    nome: 'MANUELA-SAMA'
+const union = Object.assign(user, person, {
+  // Obejto do lado direito tem preferencia, se houver conflitos
+  nome: "MANUELA-SAMA",
 });
 
+//console.log(union); ----
 
+//console.log(Object.keys(union)); ---- // Object keys
 
-console.log(union);
+const values = Object.keys(union).forEach(function (valor) {
+  union.nome = "Guilarme";
+  console.log(union[valor]); // (variavel[valor]) -> access values ​​inside the keys
+});
+
+console.log("----------------------------");
+
+// Another method for use
+for (let key in union) {
+  console.log(union[key]);
+}
