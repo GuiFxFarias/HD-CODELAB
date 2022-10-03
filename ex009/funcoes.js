220b32c41b4134e4d958c5cecc8ae95e989ce16d
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-03 12:16:06 -0300

    exercise

---
+++ ex009/funcoes.js
@@ -0,0 +1,12 @@
+// Declarar uma função
+function boasVindas() {
+  console.log("Seja bem-vindo");
+}
+
+boasVindas();
+
+const adeus = function () {
+  console.log("Adeus seres humanos");
+};
+
+adeus();
