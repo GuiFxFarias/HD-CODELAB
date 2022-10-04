ccd213f56ae1dc8eab8186a68aa69bb12cb05ee0
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-04 11:40:38 -0300

    update-exercise

--- ex004/numeros.js
+++ ex004/numeros.js
@@ -1,7 +1,12 @@
 const seculo = 20;
 const seculo2 = new Number(21);
 const seculo3 = Number(19);
+let seculo0 = "15";
 
 console.log(seculo);
 console.log(seculo2);
 console.log(seculo3);
+
+console.log(typeof seculo0, seculo0);
+seculo0 = Number(seculo0);
+console.log(typeof seculo0, seculo0);
