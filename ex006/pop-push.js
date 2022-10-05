6652222e0490678bc8a1674d9b5592b48262ea2f
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-05 20:05:18 -0300

    realize

--- ex006/pop-push.js
+++ ex006/pop-push.js
@@ -4,11 +4,16 @@ const ranking = [300, 250, 200, 500, 122, 177];
 
 const removido = ranking.pop(); //remove last value in array
 
-ranking.push(1000);
+ranking.push(1000); // add
 
+/*
 ranking.sort(function (a, b) {
   return a - b; // receive personality function (A-B crescent or B-A decrescent)
 });
 
 console.log(ranking);
 console.log(removido);
+
+*/
+
+console.log(ranking);
