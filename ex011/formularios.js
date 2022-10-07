e730b626733d0c386504fb3924adfc1470d6abdf
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-07 08:35:04 -0300

    update

--- ex011/formularios.js
+++ ex011/formularios.js
@@ -1,5 +1,24 @@
-function pegarDados() {
-  const formularos = document.querySelector("form");
+function pegarDados(evento) {
+  evento.preventDefault();
 
-  console.log(formularos);
+  const formularos = document.querySelector("form");
+  const dados = {};
+
+  [...formularos.querySelectorAll("input")].forEach((tag) => {
+    //pegar separadamente cada chave dos input
+    dados[tag.name] = tag.value;
+  });
+
+  console.log(dados);
+
+  const form = new FormData(formularos);
+
+  // Duas formas de conseguir pegar os valores de cada input
+
+  console.log(
+    form.forEach((valor, chave) => {
+      //pegar separadamente cada chave dos input
+      console.log(chave, valor);
+    })
+  );
 }
