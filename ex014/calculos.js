d3fee6f759e4fc7530d4a0a0cd8351d305dc50d1
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-10 13:53:38 -0300

    imports

--- ex014/calculos.js
+++ ex014/calculos.js
@@ -1,23 +1,24 @@
-function somar(a, b) {
+export function somar(a, b) {
   return a + b;
 }
 
-function subtrair(a, b) {
+export function subtrair(a, b) {
   return a - b;
 }
 
-function multiplicar(a, b) {
+export function multiplicar(a, b) {
   return a * b;
 }
 
-function dividir(a, b) {
+export function dividir(a, b) {
   return a / b;
 }
 
-function regraDeTres(a, b, c) {
+export function regraDeTres(a, b, c) {
   return dividir(multiplicar(c, b), a);
 }
 
+/*
 export default {
   somar,
   subtrair,
@@ -25,3 +26,4 @@ export default {
   regraDeTres,
   multiplicar,  
 };
+*/
--- ex014/mudulo-import.js
+++ ex014/mudulo-import.js
@@ -1,7 +1,10 @@
-import calculos from "./calculos.js"; // usando import para importar o codigos
+//import calculos from "./calculos.js"; // usando import para importar o codigos
 
-console.log(calculos.somar(1, 5));
-console.log(calculos.multiplicar(2, 10));
+import {somar, multiplicar} from "./calculos.js";// usando o destruct
+
+
+console.log(somar(1, 5));
+console.log(multiplicar(2, 10));
 
 /*
 console.log(objetoCalc);
