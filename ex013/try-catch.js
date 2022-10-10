6ee3092cbf5f82d18d227a6e5c334be3c43ffc40
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-10 11:35:10 -0300

    throw

---
+++ ex013/try-catch.js
@@ -0,0 +1,24 @@
+function Excecao(msg) {
+    this.message = msg;
+    this.name = 'Excecao'
+  }
+  function NaoNumeroExeception() {
+    this.message = "É necessário informar um número";
+    this.name = "NãoNúmeroExeception";
+  }
+  
+  function dividir(a, b) {
+    if (typeof a !== "number") {
+      throw new NaoNumeroExeception();
+    } else if (typeof b !== "number") {
+      throw new NaoNumeroExeception();
+    } else if (b === 0) {
+      throw new Excecao("Não pode usar o número zero para a divisão");
+    }
+    return a / b;
+  }
+  
+  console.log(dividir(10, 2));
+  console.log(dividir(10, 1));
+  console.log(dividir(10, 0));
+  
