eb579a04d5e8e14eb06043701966615f938887af
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-10 20:04:29 -0300

    update

--- ex015-express/index.js
+++ ex015-express/index.js
@@ -1,13 +1,40 @@
+const { request, response } = require("express");
 const express = require("express");
-
 const servidor = express();
 
+// Criação de banco de dados
+const neDB = require("nedb");
+const db = new neDB({
+  filename: "DataBase.db",
+  autoload: true,
+});
+
 servidor.get("/produtos", (request, response) => {
-  response.json({
-    sucesso: true,
+  db.find({}).exec((erro, dados) => {
+    if (erro) {
+      console.error(erro);
+    } else {
+      response.json(dados);
+    }
   });
 });
 
-servidor.listen(4000, () => {
+servidor.use(express.json());// ler os dados via chaves
+servidor.use(express.urlencoded({extended: true}))// ler dados enviados via post
+
+servidor.post("/produtos", (request, response) => {
+  db.insert(request.body, (erro,novoProduto)=>{
+    if(erro){
+      console.error(erro)
+    }else{
+      response.
+    }
+    
+
+  })
+  response.json(request.body); //um objeto, json
+});
+
+servidor.listen(3000, () => {
   console.log("Já está enm funcionamento");
 });
