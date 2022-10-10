6a01be35e44812f0f72389aadbfd7b617dcd0245
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-10-10 18:49:15 -0300

    update

--- ex015-express/index.js
+++ ex015-express/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const servidor = express();
 
+servidor.get("/produtos", (request, response) => {
+  response.json({
+    sucesso: true,
+  });
+});
+
 servidor.listen(4000, () => {
   console.log("Já está enm funcionamento");
 });
